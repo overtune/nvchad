d2daad08af01c51ceedce69e13758c51626c82aa
Johan Runesson <info@johanrunesson.se>
2024-06-10 14:02:38 +0200

    feat: initial commit

---
+++ lua/mappings.lua
@@ -0,0 +1,15 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jj", "<ESC>")
+map("n", "<leader>w", ":w <CR>>", { desc = "Save buffer" })
+map("n", "<leader>q", ":q <CR>>", { desc = "Quit" })
+map("n", "<leader>jj", ":bprev <CR>", { desc = "Previous buffer" })
+map("n", "<leader>kk", ":bnext <CR>", { desc = "Next buffer" })
+map("n", "<leader>cp", ":let @+ = expand('%:p') <CR>", { desc = "Copy file path" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,80 @@
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set("n", "s", api.node.open.vertical, { buffer = bufnr })
+end
+
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = {
+      -- add any options here
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    --- lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() end,
+    opts = {},
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup {
+        on_attach = my_on_attach,
+      }
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     require("nvchad.configs.lspconfig").defaults()
+  --     require "configs.lspconfig"
+  --   end,
+  -- },
+  --
+  -- {
+  -- 	"williamboman/mason.nvim",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"lua-language-server", "stylua",
+  -- 			"html-lsp", "css-lsp" , "prettier"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
