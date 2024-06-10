36e5fa72ef09f5b85859b98ff4e7e47ec60688a4
Johan Runesson <info@johanrunesson.se>
2024-06-10 14:12:38 +0200

    fix: fixing incorrect mapping for <leader> q/w.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
-map("n", "<leader>w", ":w <CR>>", { desc = "Save buffer" })
-map("n", "<leader>q", ":q <CR>>", { desc = "Quit" })
+map("n", "<leader>w", ":w <CR>", { desc = "Save buffer" })
+map("n", "<leader>q", ":q <CR>", { desc = "Quit" })
 map("n", "<leader>jj", ":bprev <CR>", { desc = "Previous buffer" })
 map("n", "<leader>kk", ":bnext <CR>", { desc = "Next buffer" })
 map("n", "<leader>cp", ":let @+ = expand('%:p') <CR>", { desc = "Copy file path" })
